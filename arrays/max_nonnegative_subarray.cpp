c37721fb3a9d85a66601fe8b2505afd03abf450c
Mareddy Sushma <36449873+sushmamareddy@users.noreply.github.com>
2020-05-06 15:04:25 +0530

    Update max_nonnegative_subarray.cpp

--- arrays/max_nonnegative_subarray.cpp
+++ arrays/max_nonnegative_subarray.cpp
@@ -1,8 +1,39 @@
 #include<bits/stdc++.h>
 using namespace std;
-vector<int> subarray(int a[],int n)
+void subarray(int a[],int n,vector<int>&v)
 {
-	
+	int cursum=0,maxsum=0;
+	int curlength=0,maxlength=0,curstart=0,maxstart=0;
+	for(int i=0;i<n;i++)
+	{
+		if(a[i]<0)
+		{
+			if((cursum>maxsum) || ((cursum==maxsum) && (curlength>maxlength)))
+			{
+				maxsum=cursum;
+				maxlength=curlength;
+				maxstart=curstart;
+			}
+			cursum=0;
+			curlength=0;
+			curstart=i+1;
+		}
+		else
+		{
+			cursum=cursum+a[i];
+			curlength++;
+		}
+	}
+	if((cursum>maxsum) || ((cursum==maxsum) && (curlength>maxlength)))
+	{
+		maxsum=cursum;
+		maxlength=curlength;
+		maxstart=curstart;
+	}
+	for(int i=maxstart;i<=maxstart+maxlength-1;i++)
+	{
+		v.push_back(a[i]);
+	}
 }
 int main()
 {
@@ -13,7 +44,8 @@ int main()
 	{
 		cin>>a[i];
 	}
-	vector<int>v=subarray(a,n);
+	vector<int>v;
+	subarray(a,n,v);
 	for(int i=0;i<v.size();i++)
 	{
 		cout<<v[i]<<"\n";
